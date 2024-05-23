5044441967d57a3b5877d16aa36769900c7ee36b
romanovio <romanovio@vtb.ru>
2024-05-23 20:32:03 +0300

    Hello, reactive world! RxJava3 with lambda

--- rx-java/src/main/java/ru/romanov/reactive/example/SomeTestMain.java
+++ rx-java/src/main/java/ru/romanov/reactive/example/SomeTestMain.java
@@ -1,40 +1,22 @@
 package ru.romanov.reactive.example;
 
 
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
 
 public class SomeTestMain {
     public static void main(String[] args) {
-        Observable<String> observable = Observable.create(sub -> {
-            sub.onNext("Hello, reactive world!");
-            sub.onComplete();
-        });
+        Disposable subscribe = Observable.create(
+                sub -> {
+                    sub.onNext("Hello, reactive world!");
+                    sub.onComplete();
+                }
+        ).subscribe(
+                System.out::println,
+                System.err::println,
+                () -> System.out.println("Done!")
+        );
 
-        Observer<String> subscriber = new Observer<>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                System.out.println(d);
-            }
-
-            @Override
-            public void onNext(@NonNull String s) {
-                System.out.println(s);
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println(throwable.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("Я закончил");
-            }
-        };
-
-        observable.subscribe(subscriber);
+        System.out.println(subscribe);
     }
 }
